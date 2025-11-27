d9bbef3cc0c699a485bb067ff9f1ddd9d2f4c3dc
tuhin <tuhin153695@gmail.com>
2025-11-27 21:58:51 +0600

    string in cpp

---
+++ Module 05 - String Class in C++/string.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "hello";
+    string s2 = "hello";
+    if(s==s2)
+    {
+        cout << "same" << endl;
+    }
+    else
+    {
+        cout << "not same" << endl;
+    }
+    return 0;
+}
+
+

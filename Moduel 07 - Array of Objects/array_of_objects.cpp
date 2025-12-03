c98363e62306862b3b0f5bb258becda93adb7415
tuhin <tuhin153695@gmail.com>
2025-12-03 11:22:29 +0600

    arrayof objects

---
+++ Moduel 07 - Array of Objects/array_of_objects.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    Student a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin.ignore();
+        getline(cin, a[i].name);
+        cin >> a[i].roll >> a[i].marks;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+    }
+
+    return 0;
+}
+
+
+// input:
+// 3
+// sakib khan
+// 15 89
+// rakib hasan
+// 18 65
+// akib ahmed
+// 40 38
+
+
+// output:
+// sakib khan 15 89
+// rakib hasan 18 65
+// akib ahmed 40 38
